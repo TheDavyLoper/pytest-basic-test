9d8b71bc229cebf05116c0cf11c64c043e36b1da
David Akpan <daviduwana@gmail.com>
2025-10-23 18:37:31 +0100

    add comment

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ def add(a,b):
     return a+b
 
 def divide(a,b):
-    raise ValueError("Cannot divide by zero")
+    raise ValueError("Cannot divide by zero") #raise an error if b is zero
     return a/b
